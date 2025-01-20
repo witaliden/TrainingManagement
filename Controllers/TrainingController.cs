042970ff4a9af71bbfe079ef3898aa410d8b4989
WD <52528405+witaliden@users.noreply.github.com>
2025-01-20 01:25:28 +0100

    Trainings implementation for employee

--- Controllers/TrainingController.cs
+++ Controllers/TrainingController.cs
@@ -1,151 +1,67 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using TrainingManagement.Models;
 using TrainingManagement.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
-namespace TrainingManagement.Controllers
+public class TrainingController : Controller
 {
-    [Authorize]
-    public class TrainingController : Controller
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<User> _userManager;
+
+    public TrainingController(ApplicationDbContext context, UserManager<User> userManager)
     {
-        private readonly ApplicationDbContext _context;
+        _context = context;
+        _userManager = userManager;
+    }
 
-        public TrainingController(ApplicationDbContext context)
+    [Authorize]
+    public async Task<IActionResult> Overview()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        var userTrainings = await _context.UserTrainings
+            .Include(ut => ut.Training)
+            .Where(ut => ut.UserId == user.Id)
+            .ToListAsync();
+
+        return View(userTrainings);
+    }
+
+    [Authorize]
+    public async Task<IActionResult> Details(int id)
+    {
+        var user = await _userManager.GetUserAsync(User);
+        var userTraining = await _context.UserTrainings
+            .Include(ut => ut.Training)
+            .FirstOrDefaultAsync(ut => ut.TrainingId == id && ut.UserId == user.Id);
+
+        if (userTraining == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        public async Task<IActionResult> Index()
+        return View(userTraining);
+    }
+
+    [Authorize]
+    [HttpPost]
+    public async Task<IActionResult> CompleteTraining(int id)
+    {
+        var user = await _userManager.GetUserAsync(User);
+        var userTraining = await _context.UserTrainings
+            .FirstOrDefaultAsync(ut => ut.TrainingId == id && ut.UserId == user.Id);
+
+        if (userTraining == null)
         {
-            return View(await _context.Trainings.ToListAsync());
+            return NotFound();
         }
 
-        [Authorize(Roles = "Admin")]
-        public IActionResult Create()
-        {
-            return View();
-        }
+        userTraining.IsCompleted = true;
+        userTraining.CompletionDateTime = DateTime.Now;
+        await _context.SaveChangesAsync();
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> Create([Bind("Title,Description,Link,DueDate")] Training training)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(training);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(training);
-        }
-
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var training = await _context.Trainings.FindAsync(id);
-            if (training == null)
-            {
-                return NotFound();
-            }
-            return View(training);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Link,DueDate")] Training training)
-        {
-            if (id != training.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(training);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TrainingExists(training.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(training);
-        }
-
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var training = await _context.Trainings
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (training == null)
-            {
-                return NotFound();
-            }
-
-            return View(training);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var training = await _context.Trainings.FindAsync(id);
-            _context.Trainings.Remove(training);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool TrainingExists(int id)
-        {
-            return _context.Trainings.Any(e => e.Id == id);
-        }
-
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> AssignTraining(int trainingId, string userId)
-        {
-            var user = await _context.Users.FindAsync(userId);
-            var training = await _context.Trainings.FindAsync(trainingId);
-
-            if (user == null || training == null)
-            {
-                return NotFound();
-            }
-
-            var userTraining = new UserTraining
-            {
-                UserId = userId,
-                TrainingId = trainingId,
-                IsCompleted = false
-            };
-
-            _context.UserTrainings.Add(userTraining);
-            await _context.SaveChangesAsync();
-
-            return RedirectToAction(nameof(Index));
-        }
+        return RedirectToAction(nameof(Index));
     }
 }
